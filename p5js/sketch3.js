cdab26a820592f53aeba373371f8356dca7dea9e
RachelKidwell <71737770+RachelKidwell@users.noreply.github.com>
2020-10-17 16:12:25 -0700

    sketch

---
+++ p5js/sketch3.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(400, 300);
+}
+
+function draw() {
+  background(0, 100, 0);
+  line(0, 50, 200, 150);
+  line(200, 150, 400, 50);
+  rect(360, 10, 30, 30);
+  fill(250, 0, 0, 200);
+  stroke(255);
+}
